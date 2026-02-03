666ec1ba98a8505d032fec049b05955888765777
UDAY ZAWARE <udayzaware09@gmail.com>
2026-02-03 20:41:22 +0530

    use of delete command

---
+++ 1.Creating and Managing Databases/deleteDataFromTable.sql
@@ -0,0 +1,10 @@
+SELECT * FROM student;
+
+INSERT INTO student VALUES (6, 'Ajit More', 256, 'Male', 2000);
+
+SELECT * FROM student;
+
+--delete data from table
+DELETE FROM student WHERE Std_ID=6;
+
+SELECT * FROM student;
